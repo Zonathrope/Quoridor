bdcb8ff2da550a6a4093490b2eb03df0acabeb24
ultcode <55034698+ultcode@users.noreply.github.com>
2021-09-26 18:37:06 +0300

    Add equals method

--- Quoridor/src/Model/CellPosition.cs
+++ Quoridor/src/Model/CellPosition.cs
@@ -18,11 +18,26 @@
 
         public static bool operator ==(CellPosition position1, CellPosition position2)
         {
+            if (position1 is null) return false;
+            if (position2 is null) return false;
             return position1.X == position2.X && position1.Y == position2.Y;
         }
         public static bool operator !=(CellPosition position1, CellPosition position2)
         {
             return !(position1 == position2);
+        }
+        protected bool Equals(CellPosition other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.
+                GetType() != this.GetType()) return false;
+            return Equals((CellPosition) obj);
         }
 
         public override string ToString()
