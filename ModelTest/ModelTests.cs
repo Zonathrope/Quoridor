852f25dcebc7888ebcbf3acbf17bf10b172f9645
ultcode <55034698+ultcode@users.noreply.github.com>
2021-09-28 04:57:30 +0300

    Add few most basic tests

---
+++ ModelTest/ModelTests.cs
@@ -0,0 +1,49 @@
+using Model.Api;
+using NUnit.Framework;
+using Moq;
+
+using Quoridor.Model;
+
+namespace ModelTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        private IGameModel _gameModel;
+        [SetUp]
+        public void Setup()
+        {
+            var player1Mock = Mock.Of<IPlayerView>();
+            var player2Mock = Mock.Of<IPlayerView>();
+            _gameModel = new GameModel(player1Mock, player2Mock);
+        }
+
+        [Test]
+        public void Game_starts()
+        {
+            Assert.DoesNotThrow(() => _gameModel.StartNewGame());
+        }
+        
+        [Test]
+        public void Game_ends()
+        {
+            Assert.DoesNotThrow(() => _gameModel.EndGame());
+        }
+        
+        [Test]
+        public void Game_starts_and_ends()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                _gameModel.StartNewGame();
+                _gameModel.EndGame();                
+            });
+        }
+
+        [Test]
+        public void Player1_makes_move()
+        {
+            
+        }
+    }
+}
