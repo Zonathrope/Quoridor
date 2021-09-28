dc7316940ae0b9d1ffe72a3bbc110b82a949fed0
ultcode <55034698+ultcode@users.noreply.github.com>
2021-09-28 05:17:12 +0300

    Add test for basic moves

--- ModelTest/ModelTests.cs
+++ ModelTest/ModelTests.cs
@@ -7,7 +7,7 @@ using Model;
 namespace ModelTest
 {
     [TestFixture]
-    public class Tests
+    public class StarEndTests
     {
         private IGameModel _gameModel;
         [SetUp]
@@ -39,11 +39,64 @@ namespace ModelTest
                 _gameModel.EndGame();                
             });
         }
+    }
+
+    [TestFixture]
+    public class BasicActionsTest
+    {
+        private IGameModel _gameModel;
+        [SetUp]
+        public void Setup()
+        {
+            var player1Mock = Mock.Of<IPlayerView>();
+            var player2Mock = Mock.Of<IPlayerView>();
+            _gameModel = new GameModel(player1Mock, player2Mock);
+            _gameModel.StartNewGame();
+        }
+        
+        private bool TestMove(PlayerNumber playerNumber, CellPosition position)
+        {
+            _gameModel.MovePlayer(playerNumber, 4, 7);
+            return _gameModel.GetPlayerPosition(playerNumber) == position;
+        }
 
         [Test]
-        public void Player1_makes_move()
+        public void Player1_move_to_correct_position_1()
         {
-            
+            Assert.IsTrue(TestMove(PlayerNumber.First, new CellPosition(4, 7)));
+        }
+        
+        [Test]
+        public void Player1_move_to_correct_position_2()
+        {
+            Assert.IsTrue(TestMove(PlayerNumber.First, new CellPosition(3, 8)));
+        }
+        
+        [Test]
+        public void Player1_move_to_correct_position_3()
+        {
+            Assert.IsTrue(TestMove(PlayerNumber.First, new CellPosition(5, 8)));
+        }
+        
+        [Test]
+        public void Player2_move_to_correct_position_1()
+        {
+            _gameModel.MovePlayer(PlayerNumber.First, 4, 7);
+            Assert.IsTrue(TestMove(PlayerNumber.Second, new CellPosition(4, 1)));
+        }
+        
+        [Test]
+        public void Player2_move_to_correct_position_2()
+        {
+            _gameModel.MovePlayer(PlayerNumber.First, 4, 7);
+            Assert.IsTrue(TestMove(PlayerNumber.Second, new CellPosition(3, 0)));
+        }
+        
+        [Test]
+        public void Player2_move_to_correct_position_3()
+        {
+            _gameModel.MovePlayer(PlayerNumber.First, 4, 7);
+            Assert.IsTrue(TestMove(PlayerNumber.Second, new CellPosition(5, 0)));
         }
     }
 }
