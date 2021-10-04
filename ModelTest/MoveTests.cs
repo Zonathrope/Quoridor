7aba257d0fe770f5f9e5fd15ed72076d6f0cd4b3
ultcode <55034698+ultcode@users.noreply.github.com>
2021-10-04 15:41:09 +0300

    Spit tests into separate files

---
+++ ModelTest/MoveTests.cs
@@ -0,0 +1,66 @@
+﻿using Model;
+using Model.Api;
+using Moq;
+using NUnit.Framework;
+
+namespace ModelTest
+{
+    [TestFixture]
+    public class MoveTests
+    {
+        private IGameModel _gameModel;
+        [SetUp]
+        public void Setup()
+        {
+            var player1Mock = Mock.Of<IPlayerView>();
+            var player2Mock = Mock.Of<IPlayerView>();
+            _gameModel = new GameModel(player1Mock, player2Mock);
+            _gameModel.StartNewGame();
+        }
+        
+        private void TestMove(PlayerNumber playerNumber, CellPosition position)
+        {
+            _gameModel.MovePlayer(playerNumber, position.X, position.Y);
+            Assert.AreEqual(_gameModel.GetPlayerPosition(playerNumber), position);
+        }
+
+        [Test]
+        public void Player1_move_to_correct_position_1()
+        {
+            TestMove(PlayerNumber.First, new CellPosition(4, 7));
+        }
+        
+        [Test]
+        public void Player1_move_to_correct_position_2()
+        {
+            TestMove(PlayerNumber.First, new CellPosition(3, 8));
+        }
+        
+        [Test]
+        public void Player1_move_to_correct_position_3()
+        {
+            TestMove(PlayerNumber.First, new CellPosition(5, 8));
+        }
+        
+        [Test]
+        public void Player2_move_to_correct_position_1()
+        {
+            _gameModel.MovePlayer(PlayerNumber.First, 4, 7);
+            TestMove(PlayerNumber.Second, new CellPosition(4, 1));
+        }
+        
+        [Test]
+        public void Player2_move_to_correct_position_2()
+        {
+            _gameModel.MovePlayer(PlayerNumber.First, 4, 7);
+            TestMove(PlayerNumber.Second, new CellPosition(3, 0));
+        }
+        
+        [Test]
+        public void Player2_move_to_correct_position_3()
+        {
+            _gameModel.MovePlayer(PlayerNumber.First, 4, 7);
+            TestMove(PlayerNumber.Second, new CellPosition(5, 0));
+        }
+    }
+}
--- ModelTest/WallPlacementTests.cs
+++ ModelTest/WallPlacementTests.cs
@@ -1,48 +1,12 @@
+﻿using Model;
 using Model.Api;
-using NUnit.Framework;
 using Moq;
-
-using Model;
+using NUnit.Framework;
 
 namespace ModelTest
 {
     [TestFixture]
-    public class StarEndTests
-    {
-        private IGameModel _gameModel;
-        [SetUp]
-        public void Setup()
-        {
-            var player1Mock = Mock.Of<IPlayerView>();
-            var player2Mock = Mock.Of<IPlayerView>();
-            _gameModel = new GameModel(player1Mock, player2Mock);
-        }
-
-        [Test]
-        public void Game_starts()
-        {
-            Assert.DoesNotThrow(() => _gameModel.StartNewGame());
-        }
-        
-        [Test]
-        public void Game_ends()
-        {
-            Assert.DoesNotThrow(() => _gameModel.EndGame());
-        }
-        
-        [Test]
-        public void Game_starts_and_ends()
-        {
-            Assert.DoesNotThrow(() =>
-            {
-                _gameModel.StartNewGame();
-                _gameModel.EndGame();                
-            });
-        }
-    }
-
-    [TestFixture]
-    public class BasicActionsTest
+    public class WallPlacementTests
     {
         private IGameModel _gameModel;
         [SetUp]
@@ -52,51 +16,6 @@ namespace ModelTest
             var player2Mock = Mock.Of<IPlayerView>();
             _gameModel = new GameModel(player1Mock, player2Mock);
             _gameModel.StartNewGame();
-        }
-        
-        private void TestMove(PlayerNumber playerNumber, CellPosition position)
-        {
-            _gameModel.MovePlayer(playerNumber, position.X, position.Y);
-            Assert.AreEqual(_gameModel.GetPlayerPosition(playerNumber), position);
-        }
-
-        [Test]
-        public void Player1_move_to_correct_position_1()
-        {
-            TestMove(PlayerNumber.First, new CellPosition(4, 7));
-        }
-        
-        [Test]
-        public void Player1_move_to_correct_position_2()
-        {
-            TestMove(PlayerNumber.First, new CellPosition(3, 8));
-        }
-        
-        [Test]
-        public void Player1_move_to_correct_position_3()
-        {
-            TestMove(PlayerNumber.First, new CellPosition(5, 8));
-        }
-        
-        [Test]
-        public void Player2_move_to_correct_position_1()
-        {
-            _gameModel.MovePlayer(PlayerNumber.First, 4, 7);
-            TestMove(PlayerNumber.Second, new CellPosition(4, 1));
-        }
-        
-        [Test]
-        public void Player2_move_to_correct_position_2()
-        {
-            _gameModel.MovePlayer(PlayerNumber.First, 4, 7);
-            TestMove(PlayerNumber.Second, new CellPosition(3, 0));
-        }
-        
-        [Test]
-        public void Player2_move_to_correct_position_3()
-        {
-            _gameModel.MovePlayer(PlayerNumber.First, 4, 7);
-            TestMove(PlayerNumber.Second, new CellPosition(5, 0));
         }
 
         [Test]
