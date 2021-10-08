d922520a0bacc18827d2f79ae0ce511d1e608c00
ultcode <55034698+ultcode@users.noreply.github.com>
2021-10-09 01:42:19 +0300

    Make CellPostion record

--- Model/DataTypes/CellPosition.cs
+++ Model/DataTypes/CellPosition.cs
@@ -2,7 +2,7 @@
 
 namespace Model.DataTypes
 {
-    public class CellPosition
+    public record CellPosition
     {
         public int X { get; }
         public int Y { get; }
@@ -15,30 +15,6 @@ namespace Model.DataTypes
             }
             X = x;
             Y = y;
-        }
-
-        public static bool operator ==(CellPosition position1, CellPosition position2)
-        {
-            if (position1 is null) return false;
-            if (position2 is null) return false;
-            return position1.X == position2.X && position1.Y == position2.Y;
-        }
-        public static bool operator !=(CellPosition position1, CellPosition position2)
-        {
-            return !(position1 == position2);
-        }
-        public bool Equals(CellPosition other)
-        {
-            return X == other.X && Y == other.Y;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.
-                GetType() != this.GetType()) return false;
-            return Equals((CellPosition) obj);
         }
 
         public override string ToString()
