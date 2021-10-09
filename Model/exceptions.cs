eafeb534f7c09f929cf66153783aabc81b45033f
ultcode <55034698+ultcode@users.noreply.github.com>
2021-10-09 19:32:54 +0300

    Add base exceptin and remove useless constructors

--- Model/exceptions.cs
+++ Model/exceptions.cs
@@ -2,60 +2,49 @@
 
 namespace Model
 {
-    //TODO make base Quoridor exception
-    public class IncorrectUserInputException: Exception
+    public class QuoridorException : Exception
     {
-        public IncorrectUserInputException(){}
+        public QuoridorException(string message): base(message){}
+        public QuoridorException(string message, Exception inner): base(message, inner){}
+    }
+
+    public class IncorrectUserInputException: QuoridorException
+    {
         public IncorrectUserInputException(string message): base(message){}
-        public IncorrectUserInputException(string message, Exception inner): base(message, inner){}
     }
 
     public class CellAlreadyTakenException : IncorrectUserInputException
     {
-        public CellAlreadyTakenException(){}
         public CellAlreadyTakenException(string message): base(message){}
-        public CellAlreadyTakenException(string message, Exception inner): base(message, inner){}
     }
     
     public class WallPlaceTakenException : IncorrectUserInputException
     {
-        public WallPlaceTakenException(){}
-        public WallPlaceTakenException(string message): base(message){}
-        public WallPlaceTakenException(string message, Exception inner): base(message, inner){}
+        public WallPlaceTakenException(String message) : base(message){}
     }
     
     public class IncorrectPlayerPositionException : IncorrectUserInputException
     {
-        public IncorrectPlayerPositionException(){}
         public IncorrectPlayerPositionException(string message) : base(message) {}
-        public IncorrectPlayerPositionException(string message, Exception inner) : base(message, inner){}
     }
     
     public class IncorrectWallPositionException : IncorrectUserInputException
     {
-        public IncorrectWallPositionException(){}
         public IncorrectWallPositionException(string message) : base(message){}
-        public IncorrectWallPositionException(string message, Exception inner) : base(message, inner){}
     }
 
     public class NoWallsLeftException : IncorrectUserInputException
     {
-        public NoWallsLeftException(){}
         public NoWallsLeftException(string message) : base(message){}
-        public NoWallsLeftException(string message, Exception inner) : base(message, inner){}
     }
 
     public class WallBlocksPathForPlayerException : IncorrectUserInputException
     {
-        public WallBlocksPathForPlayerException(){}
         public WallBlocksPathForPlayerException(string message) : base(message){}
-        public WallBlocksPathForPlayerException(string message, Exception inner) : base(message, inner){}
     }
 
     public class AnotherPlayerTurnException : IncorrectUserInputException
     {
-        public AnotherPlayerTurnException(){}
         public AnotherPlayerTurnException(string message) : base(message){}
-        public AnotherPlayerTurnException(string message, Exception inner) : base(message, inner){}
     }
 }
