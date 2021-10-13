c8b37ccf1dd6ca9c38ee2dbbf4c70e9609fc5df9
ultcode <55034698+ultcode@users.noreply.github.com>
2021-10-13 21:30:40 +0300

    Move methods around inside class

--- Model/GameModel.cs
+++ Model/GameModel.cs
@@ -6,204 +6,213 @@ using Model.Internal;
 
 namespace Model
 {
-public class GameModel: IGameModel
-{
-    public CellPosition Player1Position => _field.Player1Position;
-    public CellPosition Player2Position => _field.Player2Position;
-    public int Player1WallAmount { get; private set; }
-    public int Player2WallAmount { get; private set; }
-    public List<WallPosition> PlacedWalls => _field.PlacedWalls;
-
-    private Field _field;
-    //TODO replace with actual implementation
-    private IAStar _aStar = new AStarMock();
-    private PlayerNumber _currentPlayer;
-
-    private event Action                            GameStartedEvent;
-    private event Action                            GameEndedEvent;
-    private event Action<PlayerNumber>              PlayerWonEvent;
-    private event Action<PlayerMovedEventArgs>      PlayerMovedEvent;
-    private event Action<PlayerPlacedWallEventArgs> PlayerPlacedWallEvent;
-
-
-    public GameModel(IPlayerView player1, IPlayerView player2)
+    public class GameModel : IGameModel
     {
-        AttachEventsToPlayer(player1);
-        AttachEventsToPlayer(player2);
-        _field = new Field();
-    }
+        public CellPosition Player1Position => _field.Player1Position;
+        public CellPosition Player2Position => _field.Player2Position;
+        public int Player1WallAmount { get; private set; }
+        public int Player2WallAmount { get; private set; }
+        public List<WallPosition> PlacedWalls => _field.PlacedWalls;
 
-    private void AttachEventsToPlayer(IPlayerView player)
-    {
-        this.GameStartedEvent      += player.HandleGameStartedEvent;
-        this.GameEndedEvent        += player.HandleGameEndedEvent;
-        this.PlayerWonEvent        += player.HandlePlayerWonEvent;
-        this.PlayerMovedEvent      += player.HandlePlayerMovedEvent;
-        this.PlayerPlacedWallEvent += player.HandlePlayerPlacedWallEvent;
-    }
+        private Field _field;
 
-    public void StartNewGame()
-    {
-        _field = new Field();
-        _currentPlayer = PlayerNumber.First;
-        Player1WallAmount = GameConstants.StartWallAmount;
-        Player2WallAmount = GameConstants.StartWallAmount;
-        GameStartedEvent?.Invoke();
-    }
+        //TODO replace with actual implementation
+        private IAStar _aStar = new AStarMock();
+        private PlayerNumber _currentPlayer;
 
-    public void EndGame()
-    {
-        GameEndedEvent?.Invoke();
-    }
+        private event Action GameStartedEvent;
+        private event Action GameEndedEvent;
+        private event Action<PlayerNumber> PlayerWonEvent;
+        private event Action<PlayerMovedEventArgs> PlayerMovedEvent;
+        private event Action<PlayerPlacedWallEventArgs> PlayerPlacedWallEvent;
 
-    public void MovePlayer(PlayerNumber playerNumber, CellPosition newPosition)
-    {
-        if (!IsPlayersTurn(playerNumber))
+
+        public GameModel(IPlayerView player1, IPlayerView player2)
         {
-            throw new AnotherPlayerTurnException($"It is player {_currentPlayer} turn");
+            AttachEventsToPlayer(player1);
+            AttachEventsToPlayer(player2);
+            _field = new Field();
         }
 
-        if (!GetCellsAvailableForMove(playerNumber).Contains(newPosition))
+        private void AttachEventsToPlayer(IPlayerView player)
         {
-            throw new IncorrectPlayerPositionException(
-                $"Can't move from {_field.GetPlayerPosition(playerNumber)} to {newPosition}");
-        }
-        _field.MovePlayer(playerNumber, newPosition);
-        SwitchCurrentPlayer();
-        PlayerMovedEvent?.Invoke(new PlayerMovedEventArgs(playerNumber, newPosition));
-        if (IsOnWinningPosition(playerNumber))
-        {
-            HandleWin(playerNumber);
-        }
-    }
-
-    private void SwitchCurrentPlayer()
-    {
-        _currentPlayer = GetOppositePlayerNumber(_currentPlayer);
-    }
-
-    private static PlayerNumber GetOppositePlayerNumber(PlayerNumber playerNumber)
-    {
-        return playerNumber == PlayerNumber.First ? PlayerNumber.Second : PlayerNumber.First;
-    }
-
-    public List<CellPosition> GetCellsAvailableForMove(PlayerNumber playerNumber)
-    {
-        CellPosition playerCurrentPosition = _field.GetPlayerPosition(playerNumber);
-        CellPosition opponentPosition = GetPlayerPosition(GetOppositePlayerNumber(playerNumber));
-        List<CellPosition> reachableCells = _field.GetReachableNeighbours(playerCurrentPosition);
-        if (reachableCells.Contains(opponentPosition))
-        {
-            reachableCells.Remove(opponentPosition);
-            reachableCells.AddRange(
-                GetCellsAvailableFromFaceToFaceSituation(playerCurrentPosition, opponentPosition));
-        }
-        return reachableCells;
-    }
-
-    /// <summary>
-    /// For the Quoridor game the situation when opponents face each over face to face is
-    /// threatened using some special rules.
-    /// </summary>
-    /// <returns>Cells available for player to move due face to face situation.
-    /// Don't include cells available by regular rules</returns>
-    private List<CellPosition> GetCellsAvailableFromFaceToFaceSituation(
-        CellPosition playerPosition, CellPosition opponentPosition)
-    {
-        var availableCells = new List<CellPosition>();
-        int positionDifferenceX = opponentPosition.X - playerPosition.X;
-        int positionDifferenceY = opponentPosition.Y - playerPosition.Y;
-        // Cell behind opponent is acquired by finding next cell from player position in opponents direction
-        CellPosition cellBehindOpponent = opponentPosition.Shifted(positionDifferenceX, positionDifferenceY);
-        if (_field.WayBetweenExists(opponentPosition, cellBehindOpponent))
-        {
-            availableCells.Add(cellBehindOpponent);
-        }
-        else
-        {
-            List<CellPosition> opponentNeighbours = _field.GetReachableNeighbours(opponentPosition);
-            opponentNeighbours.Remove(playerPosition);
-            opponentNeighbours.Remove(cellBehindOpponent);
-            availableCells.AddRange(opponentNeighbours);
-        }
-        return availableCells;
-    }
-
-    public void PlaceWall(PlayerNumber playerPlacing, WallPosition wallPosition)
-    {
-        if (!IsPlayersTurn(playerPlacing))
-        {
-            throw new AnotherPlayerTurnException($"It is player {_currentPlayer} turn");
-        }
-        if (!PlayerHasWalls(playerPlacing))
-        {
-            throw new NoWallsLeftException($"Player {playerPlacing} has no walls left");
+            this.GameStartedEvent += player.HandleGameStartedEvent;
+            this.GameEndedEvent += player.HandleGameEndedEvent;
+            this.PlayerWonEvent += player.HandlePlayerWonEvent;
+            this.PlayerMovedEvent += player.HandlePlayerMovedEvent;
+            this.PlayerPlacedWallEvent += player.HandlePlayerPlacedWallEvent;
         }
 
-        _field.PlaceWall(wallPosition);
-        if (!BothPlayersHaveWayToLastLine())
+        public void StartNewGame()
         {
-            _field.RemoveWall(wallPosition);
-            var wallDirection = wallPosition.Orientation == WallOrientation.Horizontal ? "Horizontal" : "Vertical";
-            throw new WallBlocksPathForPlayerException(
-                $"{wallDirection} wall at {wallPosition.TopLeftCell} blocks way for players");
+            _field = new Field();
+            _currentPlayer = PlayerNumber.First;
+            Player1WallAmount = GameConstants.StartWallAmount;
+            Player2WallAmount = GameConstants.StartWallAmount;
+            GameStartedEvent?.Invoke();
         }
-        DecrementPlayerWallAmount(playerPlacing);
-        SwitchCurrentPlayer();
-        PlayerPlacedWallEvent?.Invoke(new PlayerPlacedWallEventArgs(playerPlacing, wallPosition));
-    }
 
-    private bool PlayerHasWalls(PlayerNumber playerNumber)
-    {
-        int wallAmount = playerNumber == PlayerNumber.First ? Player1WallAmount : Player2WallAmount;
-        return wallAmount != 0;
-    }
-
-    private void DecrementPlayerWallAmount(PlayerNumber playerNumber)
-    {
-        if (playerNumber == PlayerNumber.First)
-            Player1WallAmount--;
-        else
-            Player2WallAmount--;
-    }
-
-    private bool IsPlayersTurn(PlayerNumber playerNumber)
-    {
-        return playerNumber == _currentPlayer;
-    }
-
-    /// <summary>Checks if player is on last(relative to his start position)</summary>
-    private bool IsOnWinningPosition(PlayerNumber player)
-    {
-        CellPosition playerPosition = GetPlayerPosition(player);
-        if (player == PlayerNumber.First)
+        public void EndGame()
         {
-            return playerPosition.Y == 0;
+            GameEndedEvent?.Invoke();
         }
-        return playerPosition.Y == GameConstants.FieldSize;
-    }
 
-    private void HandleWin(PlayerNumber winner)
-    {
-        PlayerWonEvent?.Invoke(winner);
-    }
+        public void MovePlayer(PlayerNumber playerNumber, CellPosition newPosition)
+        {
+            if (!IsPlayersTurn(playerNumber))
+            {
+                throw new AnotherPlayerTurnException($"It is player {_currentPlayer} turn");
+            }
 
-    //TODO think if last line is good name
-    private bool BothPlayersHaveWayToLastLine()
-    {
-        return PlayerHasWayToLastLine(PlayerNumber.First) && PlayerHasWayToLastLine(PlayerNumber.Second);
-    }
+            if (!GetCellsAvailableForMove(playerNumber).Contains(newPosition))
+            {
+                throw new IncorrectPlayerPositionException(
+                    $"Can't move from {_field.GetPlayerPosition(playerNumber)} to {newPosition}");
+            }
 
-    private bool PlayerHasWayToLastLine(PlayerNumber playerNumber)
-    {
-        List<FieldCell> winLine = _field.GetPlayerWinLine(playerNumber).ToList();
-        FieldCell playerCell = _field.GetPlayerCell(playerNumber);
-        return winLine.Any(winCell => _aStar.WayExists(playerCell, winCell));
-    }
+            _field.MovePlayer(playerNumber, newPosition);
+            SwitchCurrentPlayer();
+            PlayerMovedEvent?.Invoke(new PlayerMovedEventArgs(playerNumber, newPosition));
+            if (IsOnWinningPosition(playerNumber))
+            {
+                HandleWin(playerNumber);
+            }
+        }
 
-    public CellPosition GetPlayerPosition(PlayerNumber playerNumber)
-    {
-        return playerNumber == PlayerNumber.First ? Player1Position : Player2Position;
+        private bool IsPlayersTurn(PlayerNumber playerNumber)
+        {
+            return playerNumber == _currentPlayer;
+        }
+
+        public List<CellPosition> GetCellsAvailableForMove(PlayerNumber playerNumber)
+        {
+            //TODO replace 
+            CellPosition playerCurrentPosition = _field.GetPlayerPosition(playerNumber);
+            CellPosition opponentPosition = GetPlayerPosition(GetOppositePlayerNumber(playerNumber));
+            List<CellPosition> reachableCells = _field.GetReachableNeighbours(playerCurrentPosition);
+            if (reachableCells.Contains(opponentPosition))
+            {
+                reachableCells.Remove(opponentPosition);
+                reachableCells.AddRange(
+                    GetCellsAvailableFromFaceToFaceSituation(playerCurrentPosition, opponentPosition));
+            }
+
+            return reachableCells;
+        }
+
+        private static PlayerNumber GetOppositePlayerNumber(PlayerNumber playerNumber)
+        {
+            return playerNumber == PlayerNumber.First ? PlayerNumber.Second : PlayerNumber.First;
+        }
+
+        public CellPosition GetPlayerPosition(PlayerNumber playerNumber)
+        {
+            return playerNumber == PlayerNumber.First ? Player1Position : Player2Position;
+        }
+
+        /// <summary>
+        /// For the Quoridor game the situation when opponents face each over face to face is
+        /// threatened using some special rules.
+        /// </summary>
+        /// <returns>Cells available for player to move due face to face situation.
+        /// Don't include cells available by regular rules</returns>
+        private List<CellPosition> GetCellsAvailableFromFaceToFaceSituation(
+            CellPosition playerPosition, CellPosition opponentPosition)
+        {
+            var availableCells = new List<CellPosition>();
+            int positionDifferenceX = opponentPosition.X - playerPosition.X;
+            int positionDifferenceY = opponentPosition.Y - playerPosition.Y;
+            // Cell behind opponent is acquired by finding next cell from player position in opponents direction
+            CellPosition cellBehindOpponent = opponentPosition.Shifted(positionDifferenceX, positionDifferenceY);
+            if (_field.WayBetweenExists(opponentPosition, cellBehindOpponent))
+            {
+                availableCells.Add(cellBehindOpponent);
+            }
+            else
+            {
+                List<CellPosition> opponentNeighbours = _field.GetReachableNeighbours(opponentPosition);
+                opponentNeighbours.Remove(playerPosition);
+                opponentNeighbours.Remove(cellBehindOpponent);
+                availableCells.AddRange(opponentNeighbours);
+            }
+
+            return availableCells;
+        }
+
+        private void SwitchCurrentPlayer()
+        {
+            _currentPlayer = GetOppositePlayerNumber(_currentPlayer);
+        }
+
+        /// <summary>Checks if player is on last(relative to his start position)</summary>
+        private bool IsOnWinningPosition(PlayerNumber player)
+        {
+            CellPosition playerPosition = GetPlayerPosition(player);
+            if (player == PlayerNumber.First)
+            {
+                return playerPosition.Y == 0;
+            }
+
+            return playerPosition.Y == GameConstants.FieldSize;
+        }
+
+        private void HandleWin(PlayerNumber winner)
+        {
+            PlayerWonEvent?.Invoke(winner);
+        }
+
+        public void PlaceWall(PlayerNumber playerPlacing, WallPosition wallPosition)
+        {
+            if (!IsPlayersTurn(playerPlacing))
+            {
+                throw new AnotherPlayerTurnException($"It is player {_currentPlayer} turn");
+            }
+
+            if (!PlayerHasWalls(playerPlacing))
+            {
+                throw new NoWallsLeftException($"Player {playerPlacing} has no walls left");
+            }
+
+            _field.PlaceWall(wallPosition);
+            if (!BothPlayersHaveWayToLastLine())
+            {
+                _field.RemoveWall(wallPosition);
+                var wallDirection = wallPosition.Orientation == WallOrientation.Horizontal ? "Horizontal" : "Vertical";
+                throw new WallBlocksPathForPlayerException(
+                    $"{wallDirection} wall at {wallPosition.TopLeftCell} blocks way for players");
+            }
+
+            DecrementPlayerWallAmount(playerPlacing);
+            SwitchCurrentPlayer();
+            PlayerPlacedWallEvent?.Invoke(new PlayerPlacedWallEventArgs(playerPlacing, wallPosition));
+        }
+
+        private bool PlayerHasWalls(PlayerNumber playerNumber)
+        {
+            int wallAmount = playerNumber == PlayerNumber.First ? Player1WallAmount : Player2WallAmount;
+            return wallAmount != 0;
+        }
+
+        //TODO think if last line is good name
+        private bool BothPlayersHaveWayToLastLine()
+        {
+            return PlayerHasWayToLastLine(PlayerNumber.First) && PlayerHasWayToLastLine(PlayerNumber.Second);
+        }
+
+        private bool PlayerHasWayToLastLine(PlayerNumber playerNumber)
+        {
+            List<FieldCell> winLine = _field.GetPlayerWinLine(playerNumber).ToList();
+            FieldCell playerCell = _field.GetPlayerCell(playerNumber);
+            return winLine.Any(winCell => _aStar.WayExists(playerCell, winCell));
+        }
+
+        private void DecrementPlayerWallAmount(PlayerNumber playerNumber)
+        {
+            if (playerNumber == PlayerNumber.First)
+                Player1WallAmount--;
+            else
+                Player2WallAmount--;
+        }
     }
 }
-}
+
