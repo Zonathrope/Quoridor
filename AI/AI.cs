97dc6c75574a77dff8d4d0b0a2ba93c188263ee4
VektorDio <vektordiod@gmail.com>
2021-10-26 19:13:20 +0300

    negamax start

---
+++ AI/AI.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Model.DataTypes;
+
+namespace AI
+{
+    public class AI
+    {
+        public int Negamax(Field position, int depth, int alpha, int beta, int color)
+        {
+            if (depth == 0 || CheckWin(position, color))
+            {
+                return Sev(position) * color;
+            }
+            var childPositions = generatePositions(position);
+            childPositions = orderPositions(childPositions);
+            var value = -9999;
+            foreach (Field childPosition in childPositions)
+            {
+                value = Math.Max(value, -Negamax(childPosition, depth - 1, -alpha, -beta, -color));
+                alpha = Math.Max(alpha, value);
+                if (alpha >= beta) break;
+            }
+            return value;
+        }
+
+        private LinkedList<Field> orderPositions(object childPositions)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private LinkedList<Field> generatePositions(Field position)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        bool CheckWin(Field position, int color)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        int Sev(Field position){
+            return 1;
+        }
+    }
+    
+    
+}
---
+++ AI/TreeNode.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+namespace AI
+
+{
+    public class TreeNode<T>
+    {
+        private T _data;
+        private readonly LinkedList<TreeNode<T>> _children;
+
+        public TreeNode(T data)
+        {
+            _data = data;
+            _children = new LinkedList<TreeNode<T>>();
+        }
+
+        public void AddChild(T data)
+        {
+            _children.AddFirst(new TreeNode<T>(data));
+        }
+
+        public TreeNode<T> GetChild(int i)
+        {
+            foreach (TreeNode<T> n in _children)
+                if (--i == 0)
+                    return n;
+            return null;
+        }
+    }
+}
