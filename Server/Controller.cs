8750d84a73151b2e415208b18fc2ac2bf9ff6091
ultcode <55034698+ultcode@users.noreply.github.com>
2021-12-11 16:33:04 +0100

    Move controller logic to separate class

---
+++ Server/Controller.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text.Json;
+using Model;
+using Model.DataTypes;
+
+namespace Server
+{
+    using RequestResponse = String;
+    internal class Controller
+    {
+        private readonly GameModel _game;
+
+        public Controller()
+        {
+            _game = new GameModel();
+            _game.StartNewGame();
+        }
+
+        public RequestResponse HandleRequest(string request)
+        {
+            Move? move = ParseMove(request);
+            ApplyMove(move);
+            return GenResponse();
+            
+        }
+
+        private Move? ParseMove(string requestJson)
+        {
+            Move? move = null;
+            try
+            {
+                move = JsonSerializer.Deserialize<PlaceWall>(requestJson);
+            }
+            catch (NotSupportedException) { }
+            try
+            {
+                move = JsonSerializer.Deserialize<Jump>(requestJson);
+            }
+            catch (NotSupportedException) { }
+            try
+            {
+                move = JsonSerializer.Deserialize<MovePlayer>(requestJson);
+            }
+            catch (NotSupportedException) { }
+            return move;
+        }
+
+        private void ApplyMove(Move? move)
+        {
+            if (move is null)
+                return;
+            switch (move)
+            {
+                case MovePlayer movePlayer:
+                    _game.MovePlayer(movePlayer.Player, movePlayer.NewPosition);
+                    break;
+                case Jump jump:
+                    _game.MovePlayer(jump.Player, jump.NewPosition);
+                    break;
+                case PlaceWall placeWall:
+                    _game.PlaceWall(placeWall.Placer, placeWall.WallPosition);
+                    break;
+            }
+        }
+
+        private RequestResponse GenResponse()
+        {
+            return JsonSerializer.Serialize(_game.GetGameState());
+        }
+    }
+}
--- Server/Server.cs
+++ Server/Server.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Text.Json;
 using System.Threading;
 using Model;
-using Model.DataTypes;
 
 namespace Server
 {
@@ -16,10 +14,10 @@ namespace Server
         private static readonly IPAddress IpAddress = IPAddress.Parse("127.0.0.1");
         private const int Port = 11000;
         private static ManualResetEvent AllDone = new (false);
-        private readonly GameModel _game = new();
+        private Controller _controller;
         public void Start()
         {
-            _game.StartNewGame();
+            _controller = new Controller();
             IPEndPoint localEndPoint = new IPEndPoint(IpAddress, Port);
             Socket listener = new(IpAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             try
@@ -66,54 +64,13 @@ namespace Server
             content = state.StrBuilder.ToString();
             if (content.Contains("<EOF>"))
             {
-                HandleReceivedData(content, socket);
+                content = content.Replace("<EOF>", "");
+                string response = _controller.HandleRequest(content);
+                Send(response + "<EOF>", socket);
                 return;
             }
             socket.BeginReceive(state.Buffer, 0,
                 State.BufferSize, 0, ReadCallback, state);
-        }
-
-        private void HandleReceivedData(string data, Socket socket)
-        {
-            Console.WriteLine($"Read {data.Length} bytes from socket\n Data: {data}");
-            data = data.Replace("<EOF>", "");
-            Move move = ParseJson(data);
-            switch (move)
-            {
-                case MovePlayer movePlayer:
-                    _game.MovePlayer(movePlayer.Player, movePlayer.NewPosition);
-                    break;
-                case Jump jump:
-                    _game.MovePlayer(jump.Player, jump.NewPosition);
-                    break;
-                case PlaceWall placeWall:
-                    _game.PlaceWall(placeWall.Placer, placeWall.WallPosition);
-                    break;
-            }
-            string gameState = JsonSerializer.Serialize(_game.GetGameState());
-            Console.WriteLine(gameState);
-            Send(gameState + "<EOF>", socket);
-        }
-
-        private static Move ParseJson(string json)
-        {
-            Move move = null;
-            try
-            {
-                move = JsonSerializer.Deserialize<PlaceWall>(json);
-            }
-            catch (NotSupportedException) { }
-            try
-            {
-                move = JsonSerializer.Deserialize<Jump>(json);
-            }
-            catch (NotSupportedException) { }
-            try
-            {
-                move = JsonSerializer.Deserialize<MovePlayer>(json);
-            }
-            catch (NotSupportedException) { }
-            return move;
         }
 
         private void Send(string data, Socket socket)
