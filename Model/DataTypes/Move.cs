4411ea71cadc2daf3d8798da3494effcf5cf15fa
ultcode <55034698+ultcode@users.noreply.github.com>
2021-12-11 13:41:42 +0100

    Add class representing move

---
+++ Model/DataTypes/Move.cs
@@ -0,0 +1,10 @@
+﻿namespace Model.DataTypes
+{
+    public abstract record Move;
+
+    public sealed record MovePlayer(PlayerNumber Player, CellPosition NewPosition) : Move;
+
+    public sealed record Jump(PlayerNumber Player, CellPosition NewPosition) : Move;
+
+    public sealed record PlaceWall(PlayerNumber Placer, WallPosition WallPosition) : Move;
+}
